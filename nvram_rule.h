2f9f8a4b853dfa6274ce1ef0bffe129d96c0175e
frankzhou <feishengfei@gmail.com>
2013-06-18 17:16:58 +0800

    master:update

--- nvram_rule.h
+++ nvram_rule.h
@@ -1,17 +1,17 @@
 #ifndef __NVRAM_RULE_H_
 #define __NVRAM_RULE_H_
 
-#define EZPLIB_BUF_LEN          8192
+#define NVRAM_BUF_LEN          8192
 
-#define EZPLIB_USE_CLI          0
-#define EZPLIB_USE_WEB          1
+#define NVRAM_USE_CLI          0
+#define NVRAM_USE_WEB          1
 
-#define EZPLIB_INVALID          -1
-#define EZPLIB_VAL_TRUNC        -2
-#define EZPLIB_NO_RULE_SET      -3
-#define EZPLIB_NO_RULE          -4
-#define EZPLIB_NO_ATTRIBUTE     -5
-#define EZPLIB_IDX_OUT_RANGE    -6
+#define NVRAM_INVALID          -1
+#define NVRAM_VAL_TRUNC        -2
+#define NVRAM_NO_RULE_SET      -3
+#define NVRAM_NO_RULE          -4
+#define NVRAM_NO_ATTRIBUTE     -5
+#define NVRAM_IDX_OUT_RANGE    -6
 
 #define RULE_SEP "|"
 #define ATTR_SEP "^"
@@ -35,7 +35,7 @@ int sep_string(char *word, const char *delim, char **idx_arr, int max_tok);
  * \brief Get a specified rule set from nvram, parse it by the RULE_SEP 
  * character, and return the nth rule.
  */
-int ezplib_get_rule(char *rule_set, int nth, 
+int nvram_get_rule(char *rule_set, int nth, 
 	char *buf, int bsize);
 
 /**
@@ -43,7 +43,7 @@ int ezplib_get_rule(char *rule_set, int nth,
  * RULE_SEP character, and return the subrule with the attribute position in
  * between start and end.
  */
-int ezplib_get_subrule(char *rule_set, int nth, 
+int nvram_get_subrule(char *rule_set, int nth, 
 	int start, int end, char *buf, int bsize);
 
 /**
@@ -51,45 +51,45 @@ int ezplib_get_subrule(char *rule_set, int nth,
  * from nvram. The rules are separated by blank character and the attributes
  * in a rule are separated by '-' character.
  */
-int ezplib_get_attr_val(char *rule_set, int nth, 
+int nvram_get_attr_val(char *rule_set, int nth, 
 	char *type, char *buf, int bsize, int use);
 
-//int ezplib_op_rule(char *rule_set, enum opcode op, int nth, char *new_rule);
+//int nvram_op_rule(char *rule_set, enum opcode op, int nth, char *new_rule);
 
 /**
  * \brief Replace the nth rule in the given rule-name as the specific rule.
  */
-int ezplib_replace_rule(char *rule_set, int nth, char *new_rule);
+int nvram_replace_rule(char *rule_set, int nth, char *new_rule);
 
 /**
  * \brief Replace the mth attribute of the nth rule in the given rule-name as 
  * the specific rule.
  */
-int ezplib_replace_attr(char *rule_set, int nth, 
+int nvram_replace_attr(char *rule_set, int nth, 
 	char *attr, char *new_attr);
 
 /**
  * \brief Append a new rule into the given rule-name.
  */
-int ezplib_append_rule(char *rule_set, char *new_rule);
+int nvram_append_rule(char *rule_set, char *new_rule);
 
 /**
  * \brief Prepend a new rule into the given rule-name.
  */
-int ezplib_prepend_rule(char *rule_set, char *new_rule);
+int nvram_prepend_rule(char *rule_set, char *new_rule);
 
 /**
  * \brief Add a new rule in the given rule-name as the specific rule.
  */
-int ezplib_add_rule(char *rule_set, int nth, char *new_rule);
+int nvram_add_rule(char *rule_set, int nth, char *new_rule);
 
 /**
  * \brief Delete nth rule in the given rule-name.
  */
-int ezplib_delete_rule(char *rule_set, int nth);
+int nvram_delete_rule(char *rule_set, int nth);
 
 /**
  * \brief Get the number of rules in the given rule-name.
  */
-int ezplib_get_rule_num(char *rule_set);
+int nvram_get_rule_num(char *rule_set);
 #endif
