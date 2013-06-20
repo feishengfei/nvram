edd2755eba4fe59529a85489c5dadbfd9a0ab408
frankzhou <feishengfei@gmail.com>
2013-06-20 10:51:18 +0800

    add lan_static_rule for goahead

--- nvram_factory.h
+++ nvram_factory.h
@@ -9,6 +9,12 @@ nvram_tuple_t nvram_factory_default[] = {
 	{ "abc", "a^0x61|b^0x62|c^0x63",                                   
 		NVRAM_NONE, 0                                                                     
 	}, 
+
+	//goahead
+	{ "lan_static_rule", "192.168.1.20^255.255.255.0^192.168.1.1^^^^^^^^^^",
+		NVRAM_NONE, 0
+	},
+
 	{0,0,0,0}
 };
 #endif
